8d995e0c543393d3045e76ce7b7baa7d5cc5651a
José-Guilherme-20 <jgscosta20@gmail.com>
2022-10-15 10:22:46 -0300

    menu-mobile

---
+++ assets/js/responsividade.js
@@ -0,0 +1,16 @@
+function menuShow() {
+  var tam = document.getElementsByTagName("header")[0];
+
+  var menuMobile = document.querySelector(".mobile-menu");
+
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "/assets/imagens/menu_white_36dp.svg";
+    tam.style.height = "auto";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src =
+      "/assets/imagens/close_white_36dp.svg";
+    tam.style.height = "auto";
+  }
+}
